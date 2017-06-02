4ad324962308f7a71ec35174f598ebe0a96508ae
winky_swl <alansiu4389@hotmail.com>
2017-06-02 16:52:36 +0800

    Initial Commit

---
+++ TabBarExample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabBarExample
+//
+//  Created by Winky_swl on 2/6/2017.
+//  Copyright © 2017年 Winky_swl. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
